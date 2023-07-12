4bafe5b1d05dc5c519048ec90245166929a16cae
Fahmi-XD <121919479+Fahmi-XD@users.noreply.github.com>
2023-07-12 17:23:49 +0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Loading Page //
 document.onreadystatechange = function() {
     if (document.readyState !== "complete") {
         document.querySelector("body").style.visibility = "hidden";
@@ -8,6 +9,7 @@ document.onreadystatechange = function() {
     }
 };
 
+// Scrool Text //
 let num = 2, num2 = 1;
 
 setInterval(function() {
@@ -29,3 +31,35 @@ setInterval(function() {
         num = 1;
     }
 }, 2500);
+
+// Music //
+const listImg = document.querySelector(".playlist .music img");
+const list = document.querySelectorAll(".playlist .list");
+
+for (let i = 0; i < list.length; i++) {
+    list[i].addEventListener("click", function() {
+        const getImgSrc = document.querySelectorAll(".playlist .list .img img");
+        const getListText = document.querySelectorAll(".playlist .list .info h3");
+        const setMusicText = document.querySelector(".playlist .music h3");
+        listImg.src = getImgSrc[i].src;
+        setMusicText.innerText = getListText[i].innerText;
+        for (let i = 0; i < list.length; i++) {
+            list[i].style = "background: rgba(26, 26, 26, 1);";
+        }
+        list[i].style = "background: darkorange;";
+    });
+}
+
+// navbar //
+const navbar = document.querySelector("#navbar");
+const navbarBody = document.querySelector("#navbar #body");
+const hero = document.querySelector(".hero-section");
+const aboutMe = document.querySelector(".about-me");
+
+navbarBody.addEventListener("click", function() {
+    navbar.classList.toggle("show");
+    if (navbar.classList[1]) {
+        hero.addEventListener("click", function() { navbar.classList.remove("show"); });
+        aboutMe.addEventListener("click", function() { navbar.classList.remove("show"); });
+    };
+});
